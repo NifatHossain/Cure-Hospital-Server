ae61527ed6833dc3e3dbe6ce5c050ad68afb0d3f
NifatHossain <nifathossain1480@gmail.com>
2024-05-15 01:43:49 +0600

    created api for getting card Details

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 //mondodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtdunhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,12 @@ async function run() {
     app.get('/alltreatments', async(req,res)=>{
         const cursor = treatmentCollection.find();
         const result= await cursor.toArray()
+        res.send(result)
+    })
+    app.get('/carddetails/:id', async(req,res)=>{
+        const newId= req.params.id;
+        const query = { _id: new ObjectId(newId) };
+        const result= await treatmentCollection.findOne(query);
         res.send(result)
     })
     // Send a ping to confirm a successful connection
